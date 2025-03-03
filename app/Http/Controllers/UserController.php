178d0c1009eb2b8bc3ba05f818c06b00b7d1cb2e
Rendha <rendhazuper@gmail.com>
2025-03-03 09:17:14 +0700

    Praktikum 4 - Fillable

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -13,11 +13,13 @@ class UserController extends Controller
    {
     $data = [
 
-        'nama' => 'pelanggan',
+        'level_id' => 2,
+        'username' => 'manager_3',
+        'nama' => 'Manager 3',
+        'password' => Hash::make('12345')
 
     ];
-      UserModel::where('username','customer-1')->update($data);
-
+      UserModel::create($data);
         $user = UserModel::all();
         return view('user',['data' => $user]);
    }
